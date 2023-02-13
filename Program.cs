d37556ddaea6697ad219f940b90311978791bfcc
Lexizli <pho_lex-r@yahoo.com>
2023-02-13 21:07:49 +0300

    method for selecting strings from array, first variant

--- Program.cs
+++ Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+/* selecting from array of strings only shorter then 4 characters */ 
+void SelectOnlyThree(ref string[] arrayInput, ref string[] arrayOutput)
+{
+    int j = 0;
+    for (int i = 0; i < arrayInput.Length; i++)
+    {
+        if(arrayInput[i].Length  < 4)
+        {
+            arrayOutput[j] = arrayInput[i];
+            Array.Resize(ref arrayOutput, arrayOutput.Length + 1);
+            j++;
+        }
+    }
+
+}
+
+void WriteArray(string[] arrayInput)
+{
+    for (int i = 0; i < arrayInput.Length; i++)
+    {
+        Console.WriteLine(arrayInput[i]);
+    }
+
+}
+
+//string[] arrayInput = new string[1];
+string[] arrayOutput = new string[1];
+string[] arrayInput = new string[] { "Sun", "Monday", "Tue", "Wednsday", "Thu", "Friday", "Sat" };
+
+SelectOnlyThree(ref arrayInput, ref arrayOutput);
+WriteArray(arrayOutput);
